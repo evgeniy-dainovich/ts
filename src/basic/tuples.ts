690ad8575170cf4eae083b5c359113c438800c5f
Evgeniy Dainovich <evg.dainovich@gmail.com>
2021-09-17 11:18:56 +0300

    add severals types

--- src/basic/tuples.ts
+++ src/basic/tuples.ts
@@ -1,7 +1,7 @@
 type Coordinate = [number, number, number?]
 
-const cord1 = [1, 2, 3]
-const cord2 = [1, 2]
+const cord1: Coordinate = [1, 2, 3]
+const cord2: Coordinate = [1, 2]
 
 type Coordinate1 = [number, number, ...string[]] // show that there might be a lot of values
-const cord3 = [1, 2, '3', '3', '3']
+const cord3: Coordinate1 = [1, 2, '3', '3', '3']
